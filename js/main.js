7b3678da56d6f512669fa86b9f154adff7b58b9f
manikandan071 <manidhiyamech07167@gmail.com>
2022-10-10 17:26:20 +0530

    add to carts

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 let product=[
     {id:1,brand:"redmi",model:"Note 10",price:15000,qty:1},
-    {id:2,brand:"realmi",model:"Note 10",price:15000,qty:1},
+    {id:2,brand:"realme",model:"Note 10",price:15000,qty:1},
     {id:3,brand:"oppo",model:"Note 10",price:15000,qty:1},
     {id:4,brand:"oneplus",model:"Note 10",price:15000,qty:1},
     {id:5,brand:"vivo",model:"Note 10",price:15000,qty:1}
     ];
 
 
-    let addCart=[];
 
     // let select=adding.find(function(a){
     //     console.log(a);
@@ -24,15 +23,18 @@ let product=[
     // })
     // console.log(addCart);
 
+let display=document.getElementById("display");
+
+let addCart=[];
+
 function add(id){
-    console.log(id);
+
 
     var res=addCart.find(function(items){
         return items.id === id;
     });
     if(res){
         var temp=addCart.map(function(items){
-            console.log(items);
             if(items.id === id){
                 items.qty++;
                 return items;
@@ -51,66 +53,28 @@ function add(id){
         })
         addCart.push(temp);
     }
+    console.log(addCart);
+    // addCart.forEach(myFunction);
+    myFunction(addCart);
+
 }
 
 
-for (var i=1; i<=product.length; i++){
-    add(i);
-}
+// for (var i=1; i<=product.length; i++){
+//     add(i);
+// }
 // add(3);
-console.log(addCart);
+let addingList=document.getElementById("suffer");
 
-// addCart.map((item,index)=>{
-//     return(
-//          document.getElementById("get").innerHTML=item.id+item.brand+item.model+item.price+item.qty
-//     );
-// });
-let display=document.getElementById("display");
-addCart.forEach(myFunction);
+addingList.addEventListener("click",function(){
+    let enterId=parseInt(prompt("enterId"));
 
-function myFunction(items,index){
-//     var text =+ index + ";" + items.id + ";" + items.brand;
-// console.log(text);
-    let colm=document.createElement("div");
-    colm.setAttribute("class","col-3");
-    let card=document.createElement("div");
-    card.setAttribute("class","card");
-    colm.append(card);
-    let cardBdy=document.createElement("div");
-    cardBdy.setAttribute("class","card-body");
-    card.append(cardBdy);
-    let brand=document.createElement("h4");
-    brand.innerText=items.brand;
-    cardBdy.append(brand);
-    let model=document.createElement("h4");
-    model.innerText=items.model;
-    cardBdy.append(model);
-    let price=document.createElement("h4");
-    price.innerText=items.price;
-    cardBdy.append(price);
-    
-    let dicreses=document.createElement("button");
-    dicreses.setAttribute("class","dicreses btn");
-    let dicIcon=document.createElement("i");
-    dicIcon.setAttribute("class","icofont-minus");
-    dicreses.append(dicIcon);
-    cardBdy.append(dicreses);
-    let qty=document.createElement("span");
-    qty.setAttribute("class","quantity")
+    add(enterId);
+})
 
-    qty.innerText=items.qty;
-    cardBdy.append(qty);
-    let increase=document.createElement("button");
-    increase.setAttribute("class","increase btn");
-    let incIcon=document.createElement("i");
-    incIcon.setAttribute("class","icofont-plus");
-    increase.append(incIcon);
-    cardBdy.append(increase);
-    display.append(colm);
-}
 function dicreass(i){
     let getPrnt=this.parentNode.parentNode.parentNode;
-    let qty=getPrnt.getElementsByClassName("quantity")[0];
+    let qty=getPrnt.getElementsByClassName("quantity")[0]; 
     let num=parseInt(qty.innerText);
     if(1 < num){
         qty.innerText=num-1;
@@ -123,15 +87,62 @@ function increment(j){
     let getPrnt=this.parentNode;
     let qty=getPrnt.getElementsByClassName("quantity")[0];
     let num=parseInt(qty.innerText);
-    console.log(getPrnt,num);
     qty.innerText=num+1;
 }
 
-let dicreses=document.getElementsByClassName("dicreses");
-let increase=document.getElementsByClassName("increase");
-for (var i=0 ; i<dicreses.length ; i++){
-    dicreses[i].addEventListener("click",dicreass);
+
+function myFunction(items){
+
+
+    display.innerHTML="";
+
+    for(var i=0; i<items.length; i++){
+
+    let colm=document.createElement("div");
+    colm.setAttribute("class","col-3");
+    let card=document.createElement("div");
+    card.setAttribute("class","card");
+    colm.append(card);
+    let cardBdy=document.createElement("div");
+    cardBdy.setAttribute("class","card-body");
+    card.append(cardBdy);
+    let brand=document.createElement("h4");
+    brand.innerText=items[i].brand;
+    cardBdy.append(brand);
+    let model=document.createElement("h4");
+    model.innerText=items[i].model;
+    cardBdy.append(model);
+    let price=document.createElement("h4");
+    price.innerText=items[i].price;
+    cardBdy.append(price);
+    
+    let dicreses=document.createElement("button");
+    dicreses.setAttribute("class","dicreses btn");
+    let dicIcon=document.createElement("i");
+    dicIcon.setAttribute("class","icofont-minus");
+    dicreses.append(dicIcon);
+    cardBdy.append(dicreses);
+    let qty=document.createElement("span");
+    qty.setAttribute("class","quantity")
+
+    qty.innerText=items[i].qty;
+    cardBdy.append(qty);
+    let increase=document.createElement("button");
+    increase.setAttribute("class","increase btn");
+    let incIcon=document.createElement("i");
+    incIcon.setAttribute("class","icofont-plus");
+    increase.append(incIcon);
+    cardBdy.append(increase);
+    display.append(colm);
+    }
+    let dicreses=document.getElementsByClassName("dicreses");
+    let increase=document.getElementsByClassName("increase");
+    for (var i=0 ; i<dicreses.length ; i++){
+        dicreses[i].addEventListener("click",dicreass);
+    }
+    for(var j=0; j < increase.length; j++){
+        increase[j].addEventListener("click",increment);
 }
-for(var j=0; j < increase.length; j++){
-    increase[j].addEventListener("click",increment)
 }
+
+
