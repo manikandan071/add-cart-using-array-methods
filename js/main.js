b52c1489c5d39e7a2d2474ac1ed4b9af99d3a5d7
manikandan071 <manidhiyamech07167@gmail.com>
2022-10-11 22:45:30 +0530

    add to cart

--- js/main.js
+++ js/main.js
@@ -1,35 +1,98 @@
 let product=[
-    {id:1,brand:"redmi",model:"Note 10",price:15000,qty:1},
-    {id:2,brand:"realme",model:"Note 10",price:15000,qty:1},
-    {id:3,brand:"oppo",model:"Note 10",price:15000,qty:1},
-    {id:4,brand:"oneplus",model:"Note 10",price:15000,qty:1},
-    {id:5,brand:"vivo",model:"Note 10",price:15000,qty:1}
+    {id:1,brand:"redmi",model:"Note 10",price:14999,qty:1},
+    {id:2,brand:"realme",model:"C35",price:11999,qty:1},
+    {id:3,brand:"oppo",model:"K10",price:14990,qty:1},
+    {id:4,brand:"oneplus",model:"Nord 2T 5G",price:33999,qty:1},
+    {id:5,brand:"vivo",model:"T1 5G",price:15990,qty:1}
     ];
 
 
-
-    // let select=adding.find(function(a){
-    //     console.log(a);
-    //     let check=addCart.find(function(b){
-    //         console.log(b);
-    //         if(a.id === b.id){
-    //            return b.qty++;
-    //            console.log("mani")
-    //         }
-    //         else{
-    //             return addCart.push(a);
-    //         }
-    //     })
-    // })
-    // console.log(addCart);
-
 let display=document.getElementById("display");
+let incmBtn=document.getElementById("increment");
+let dicmBtn=document.getElementById("dicrement");
 
-let addCart=[];
 
+var addCart=[];
+
+function myFunction(items){
+    display.innerHTML="";
+
+    items.forEach(function(product){
+        let colm=document.createElement("div");
+    colm.setAttribute("class","col-3");
+    let card=document.createElement("div");
+    card.setAttribute("class","card mt-4");
+    colm.append(card);
+    let cardBdy=document.createElement("div");
+    cardBdy.setAttribute("class","card-body");
+    card.append(cardBdy);
+    
+    let brand=document.createElement("h4");
+    brand.innerText=product.brand;
+    cardBdy.append(brand);
+    let model=document.createElement("h4");
+    model.innerText=product.model;
+    cardBdy.append(model);
+    let price=document.createElement("h4");
+    price.innerText=product.price;
+    cardBdy.append(price);
+    
+    let qty=document.createElement("span");
+    qty.setAttribute("class","quantity")
+    qty.innerText=product.qty;
+    cardBdy.append(qty);
+    
+    display.append(colm);
+    })
+}
+
+
+function adding(id){
+    console.log(id);
+    let temp=addCart.map(function(item){
+        if(item.id === id){
+           return{
+            ...item,
+            qty:item.qty+1,
+           }
+        }
+        else return item;
+    })
+    addCart=temp;
+    myFunction(temp);
+    console.log("temp",temp);
+}
+
+function deleting(id){     
+
+    const selectProduct=addCart.find(function(item){
+        return item.id === id;
+    })
+    if(selectProduct){
+        if(selectProduct.qty === 1){
+            const temp=addCart.filter((item) => item.id !== id);
+            console.log(temp);
+            addCart=temp;
+            myFunction(temp);
+
+        }
+        else{
+            const temp=addCart.map((item) =>{
+                if(item.id === id){
+                    return {
+                        ...item,
+                        qty:item.qty-1,
+                    }
+                }
+                else return item;
+            });
+            addCart=temp;
+            myFunction(temp);
+            console.log("temp",temp);
+        }
+    }
+}
 function add(id){
-
-
     var res=addCart.find(function(items){
         return items.id === id;
     });
@@ -54,95 +117,21 @@ function add(id){
         addCart.push(temp);
     }
     console.log(addCart);
-    // addCart.forEach(myFunction);
     myFunction(addCart);
 
 }
-
-
-// for (var i=1; i<=product.length; i++){
-//     add(i);
-// }
-// add(3);
 let addingList=document.getElementById("suffer");
 
 addingList.addEventListener("click",function(){
     let enterId=parseInt(prompt("enterId"));
-
     add(enterId);
 })
-
-function dicreass(i){
-    let getPrnt=this.parentNode.parentNode.parentNode;
-    let qty=getPrnt.getElementsByClassName("quantity")[0]; 
-    let num=parseInt(qty.innerText);
-    if(1 < num){
-        qty.innerText=num-1;
-    }
-    else{
-        getPrnt.remove();
-    }
-}
-function increment(j){
-    let getPrnt=this.parentNode;
-    let qty=getPrnt.getElementsByClassName("quantity")[0];
-    let num=parseInt(qty.innerText);
-    qty.innerText=num+1;
-}
-
-
-function myFunction(items){
-
-
-    display.innerHTML="";
-
-    for(var i=0; i<items.length; i++){
-
-    let colm=document.createElement("div");
-    colm.setAttribute("class","col-3");
-    let card=document.createElement("div");
-    card.setAttribute("class","card");
-    colm.append(card);
-    let cardBdy=document.createElement("div");
-    cardBdy.setAttribute("class","card-body");
-    card.append(cardBdy);
-    let brand=document.createElement("h4");
-    brand.innerText=items[i].brand;
-    cardBdy.append(brand);
-    let model=document.createElement("h4");
-    model.innerText=items[i].model;
-    cardBdy.append(model);
-    let price=document.createElement("h4");
-    price.innerText=items[i].price;
-    cardBdy.append(price);
-    
-    let dicreses=document.createElement("button");
-    dicreses.setAttribute("class","dicreses btn");
-    let dicIcon=document.createElement("i");
-    dicIcon.setAttribute("class","icofont-minus");
-    dicreses.append(dicIcon);
-    cardBdy.append(dicreses);
-    let qty=document.createElement("span");
-    qty.setAttribute("class","quantity")
-
-    qty.innerText=items[i].qty;
-    cardBdy.append(qty);
-    let increase=document.createElement("button");
-    increase.setAttribute("class","increase btn");
-    let incIcon=document.createElement("i");
-    incIcon.setAttribute("class","icofont-plus");
-    increase.append(incIcon);
-    cardBdy.append(increase);
-    display.append(colm);
-    }
-    let dicreses=document.getElementsByClassName("dicreses");
-    let increase=document.getElementsByClassName("increase");
-    for (var i=0 ; i<dicreses.length ; i++){
-        dicreses[i].addEventListener("click",dicreass);
-    }
-    for(var j=0; j < increase.length; j++){
-        increase[j].addEventListener("click",increment);
-}
-}
-
-
+incmBtn.addEventListener("click",function(){
+    let num=parseInt(prompt("enterId"));
+    adding(num);
+    num="";
+});
+dicmBtn.addEventListener("click",function(){
+    let num=parseInt(prompt("enterId"));
+    deleting(num);
+});
